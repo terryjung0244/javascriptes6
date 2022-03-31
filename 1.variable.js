b45cd3f5104276f28e08cacaac764c654bc8cd38
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-30 22:43:15 -0400

    variable.js

---
+++ 1.variable.js
@@ -0,0 +1,20 @@
+//Memory
+//Computer
+//input -> process -> output, 입력 -> 처리 -> 출력
+
+//Variable 변수: 값을 저장하는 공간, 자료를 저장할 수 있는 이름이 주어진 기억장소
+//Variable declaration 변수 선언
+//Variable declaration and assignment 변수 선언과 값의 할당
+//Value reassigning 값의 재할당
+
+let a = 0;
+console.log(a);
+
+a = 1;
+console.log(a);
+
+let b;
+console.log(b);
+
+b = 2;
+console.log(b);
